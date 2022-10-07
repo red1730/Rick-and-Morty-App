89f9363e1b05b3739c683cf81928d5e6402e8239
red 1730 <ducret.raul@gmail>
2022-10-07 12:21:59 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import Navbar from "./components/Navbar.js";
+import Characters from "./components/Characters";
+import Pagination from "./components/Pagination";
 
 function App() {
+  const [characters, setCharacters] = useState([]);
+  const initialUrl = "https://rickandmortyapi.com/api/character";
+  const [info, setInfo] = useState({});
+
+  const fetchCharacters = (url) => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setCharacters(data.results);
+        setInfo(data.info);
+      })
+      .catch((error) => console.log(error));
+  };
+
+  const onPrevius = () => {
+    fetchCharacters(info.prev);
+  };
+  const onNext = () => {
+    fetchCharacters(info.next);
+  };
+
+  useEffect(() => {
+    fetchCharacters(initialUrl);
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar brand="Rick And Morty App" />
+
+      <div className="container mt-5">
+        <Pagination
+          prev={info.prev}
+          next={info.next}
+          onPrevius={onPrevius}
+          onNext={onNext}
+        />
+        <Characters characters={characters} />
+        <Pagination 
+          prev={info.prev}
+          next={info.next}
+          onPrevius={onPrevius}
+          onNext={onNext}/>
+      </div>
+    </>
   );
 }
 
---
+++ src/components/Characters.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Characters = ({ characters = [] }) => {
+  return (
+    <div className="row">
+      {characters.map((items, index) => (
+        <div key={index} className="col mb-4">
+          <div className="card" style={{ minWidth: "200px" }}>
+            <img src={items.image} alt="" />
+            <div className="card-boby">
+              <h5 classNAme="card-title">{items.name}</h5>
+              <hr />
+              <p>Specie: {items.species}</p>
+              <p>Location: {items.location.name}</p>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Characters;
